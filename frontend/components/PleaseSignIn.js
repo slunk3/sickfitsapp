fb957a172637947655ad37e45530521812a0b3a4
Nick Boyd <nick.boyd@taistech.com>
2019-08-19 16:25:33 -0500

    Add permissions mgmt

---
+++ frontend/components/PleaseSignIn.js
@@ -0,0 +1,27 @@
+import { Query } from 'react-apollo';
+import PropTypes from 'prop-types';
+import { CURRENT_USER_QUERY } from './User';
+import SignIn from './SignIn';
+
+const PleaseSignIn = props => (
+  <Query query={CURRENT_USER_QUERY}>
+    {({ data, loading }) => {
+      if (loading) return <p>Loading...</p>;
+      if (!data.me) {
+        return (
+          <div>
+            <p>Please sign in before continuing</p>
+            <SignIn />
+          </div>
+        );
+      }
+      return props.children;
+    }}
+  </Query>
+);
+
+PleaseSignIn.propTypes = {
+  children: PropTypes.object.isRequired,
+};
+
+export default PleaseSignIn;
